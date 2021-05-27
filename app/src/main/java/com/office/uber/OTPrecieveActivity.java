755a9810d760d1294e8c16ae18e5a23c680ad686
ssaannwwaall <khansanwal922@gmail.com>
2021-05-27 16:15:25 +0500

    basic structure

--- app/src/main/java/com/office/uber/OTPrecieveActivity.java
+++ app/src/main/java/com/office/uber/OTPrecieveActivity.java
@@ -4,18 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OTPrecieveActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
-
-
-
+        setContentView(R.layout.activity_o_t_precieve);
     }
 }
--- app/src/main/java/com/office/uber/SplashActivity.java
+++ app/src/main/java/com/office/uber/SplashActivity.java
@@ -2,22 +2,50 @@ package com.office.uber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+
+import com.office.uber.helpers.Helper;
 
 public class SplashActivity extends AppCompatActivity {
 
 
 
+    private Handler handler;
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         textView=findViewById(R.id.tv_splash);
+        handler=new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(Helper.isInternetAvailable(SplashActivity.this))
+                {
+                    //Helper.isUserAvailable()
+                    if(true)
+                    {
+                        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Helper.showSnackBar(findViewById(android.R.id.content),"Login please");
+                        //intent
+                    }
+                }else {
+                    Helper.showSnackBar(findViewById(android.R.id.content),"Check your internet connection");
+                }
+
+            }
+        },2000);
 
 
-        //its comment just for testing
+
 
 
 
